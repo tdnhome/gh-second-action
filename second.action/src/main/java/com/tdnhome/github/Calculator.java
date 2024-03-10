5e516b7994996771c05801862f172a0f14fe5cbf
Trung Nguyen <trungn@fico.com>
2024-03-10 14:46:20 -0700

    Initial commit

---
+++ second.action/src/main/java/com/tdnhome/github/Calculator.java
@@ -0,0 +1,11 @@
+package com.tdnhome.github;
+
+/**
+ * A very important implementation of a Calculator
+ */
+
+public class Calculator {
+    public double add(double a, double b) {
+      return a+b;
+    }
+}
---
+++ second.action/src/test/java/com/tdnhome/github/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.tdnhome.github;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+  @Test
+  public void addTest() {
+    Calculator calculator = new Calculator();
+    Assert.assertEquals(calculator.add(0, 0), 0);
+  }
+}
